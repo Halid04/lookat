c307f79daee1359b2b465b6dad0cfb9022e3b926
Halid Cisse <77756563+Halid04@users.noreply.github.com>
2024-01-20 09:21:23 +0100

    Update SearchPage.jsx

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -10,7 +10,7 @@ const SearchPage = ({ theme, closePopup }) => {
     headers: {
       accept: "application/json",
       Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMmFlNzZhMjc2M2I0ZmI5ODZlNTU4ZWYwZmQ3MmY0MiIsInN1YiI6IjY1NTNjNjI0NTM4NjZlMDBmZjA1ZmQ5YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-6rC-PtHGkaLvToWNnErBnyqMkfx9FkK5YlL6n5zmvo",
+        "",
     },
   };
 
