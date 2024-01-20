a8a9dfa1456cd963aa6ce944e3095563395718f3
Halid Cisse <77756563+Halid04@users.noreply.github.com>
2024-01-20 09:20:49 +0100

    Update Content.jsx

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -29,7 +29,7 @@ const Content = ({
     headers: {
       accept: "application/json",
       Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMmFlNzZhMjc2M2I0ZmI5ODZlNTU4ZWYwZmQ3MmY0MiIsInN1YiI6IjY1NTNjNjI0NTM4NjZlMDBmZjA1ZmQ5YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-6rC-PtHGkaLvToWNnErBnyqMkfx9FkK5YlL6n5zmvo",
+        "",
     },
   };
 
